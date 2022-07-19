7d0a97f7197083acc4765f4f159a1aa72a6df831
Anamaria Camarasan <anamaria.finn0@gmail.com>
2022-07-19 15:56:18 +0300

    3.7

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,22 @@
 const { response } = require("express");
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+
+app.use(
+  morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      JSON.stringify(req.body),
+    ].join(" ");
+  })
+);
 
 let persons = [
   {

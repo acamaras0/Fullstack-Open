bf7c84d505433b569be9bec1d73470e1ad093587
Anamaria Camarasan <anamaria.finn0@gmail.com>
2022-07-22 13:00:11 +0300

    make it better

--- models/person.js
+++ models/person.js
@@ -1,36 +1,37 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 
 mongoose
   .connect(url)
   .then((result) => {
-    console.log("connected to MongoDB");
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
-    type:String,
-    minLength:3,
-    required: true
+    type: String,
+    minLength: 3,
+    required: true,
   },
   number: {
-    type:String,
-    minLength:8,
-    required: true}
-});
-
-personSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    type: String,
+    minLength: 8,
+    required: true,
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+module.exports = mongoose.model('Person', personSchema)

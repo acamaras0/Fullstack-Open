7ceced7cf44e2e1e49f157f28728b29cc1ef5d46
Anamaria Camarasan <anamaria.finn0@gmail.com>
2023-01-31 20:52:51 +0200

    4.8

---
+++ part4/blog/tests/blog_api.test.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+
+const api = supertest(app);
+
+test("blogs are returned as json", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+}, 100000);
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+
+test("there is one blog", async () => {
+  const response = await api.get("/api/blogs");
+
+  expect(response.body).toHaveLength(1);
+});
+
+test("the first blog author is Edsger W. Dijkstra", async () => {
+  const response = await api.get("/api/blogs");
+
+  expect(response.body[0].author).toBe("Edsger W. Dijkstra");
+});

3e9dec9de3481ce42d14ae7b917546568ea670b6
Anamaria Camarasan <anamaria.finn0@gmail.com>
2023-03-21 15:15:25 +0200

    5.6-5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import React, { useState } from "react";
 
+const Blog = ({ blog }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const handleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <span>
+        {blog.title} {blog.author}
+      </span>
+      <button onClick={() => handleVisibility()}>
+        {isVisible ? "hide" : "view"}
+      </button>
+      {isVisible ? (
+        <div>
+          <span>{blog.url}</span>
+          <br />
+          <span>
+            {blog.likes} <button>like</button>
+          </span>
+          <br />
+          <span>{blog.author}</span>
+        </div>
+      ) : null}
+    </div>
+  );
+};
 export default Blog;
